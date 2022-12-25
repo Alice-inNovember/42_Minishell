70cb65c65849deb6a93fa0d08e8137db2399a88f
Clearsu <clearwater1219@gmail.com>
2022-12-25 21:14:30 +0900

    [Test] : changed test.c

--- testcodes/test.c
+++ testcodes/test.c
@@ -6,11 +6,14 @@
 /*   By: junlee2 <junlee2@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 13:54:41 by junlee2           #+#    #+#             */
-/*   Updated: 2022/12/21 13:55:04 by junlee2          ###   ########seoul.kr  */
+/*   Updated: 2022/12/25 21:13:53 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 int	main(void)
 {
+	printf("test\n");
 	return (0);
 }
