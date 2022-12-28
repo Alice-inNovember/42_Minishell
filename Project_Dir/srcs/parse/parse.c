5a16ae90a30f4cf8a057adad67846f038e733114
Clearsu <clearwater1219@gmail.com>
2022-12-28 18:22:16 +0900

    Feat : developing parser - jincpark

--- Project_Dir/srcs/parse/parse.c
+++ Project_Dir/srcs/parse/parse.c
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 13:45:08 by jincpark          #+#    #+#             */
-/*   Updated: 2022/12/28 04:17:33 by jincpark         ###   ########.fr       */
+/*   Updated: 2022/12/28 18:21:52 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,16 +32,34 @@
  * 		char			*value;
  * }
  *
- * t_proc_data
+ * t_redir
  * {
- * 		t_list	cmd_lst;
- * 		t_list	redir_lst;
- * 		t_list	envp_lst;
- * 		...
+ * 		t_type	type;
+ * 		char	*fname;
  * }
+ *
+typedef struct s_envp
+{
+	char		*key;
+	char		*value;
+}	t_envp;
+
+typedef struct s_proc_data
+{
+	t_list	cmd_lst;
+	t_list	redir_lst;
+}	t_proc_data;
+
+typedef struct s_data
+{
+	t_list	token_lst;
+	t_list	envp_lst;
+	t_list	proc_data_lst;
+}	t_data;
+
  */
 
-int	is_redir(t_node *node)
+int	is_redir(t_data *data, t_node *node)
 {
 	if (node->type == T_LESS || node->type == T_GREAT
 		|| node->type == T_DESS || node->type == T_DGREAT)
@@ -53,41 +71,45 @@ int	is_redir(t_node *node)
 	return (0);
 }
 
-void	parse_cmd_prefix(t_proc_data *proc_data, t_node *first, t_node *last)
+void	parse_cmd_prefix(t_data *data, t_proc_data *proc_data, t_node *first, t_node *last)
 {
 	if (last->next == first) // prefix가 없는 경우
 		return ;
-	parse_cmd_prefix(proc_data, first, last->prev->prev);
-	parse_io_redirect(proc_data, last->prev, last);
+	parse_cmd_prefix(data, proc_data, first, last->prev->prev);
+	parse_io_redirect(data, proc_data, last->prev, last);
 }
 
-void	parse_cmd_suffix(t_proc_data *proc_data, t_node *first, t_node *last)
+void	parse_cmd_suffix(t_data *data, t_proc_data *proc_data, t_node *first, t_node *last)
 {
 	if (first->prev == last) // suffix가 없는 경우
 		return ;
-	if (is_redir(last->prev))
+	if (is_redir(data, last->prev))
 	{
 		// REDIR WORD 인 경우
-		parse_cmd_suffix(proc_data, first, last->prev->prev);
-		parse_io_redirect(proc_data, last->prev, last);
+		parse_cmd_suffix(data, proc_data, first, last->prev->prev);
+		parse_io_redirect(data, proc_data, last->prev, last);
 	}
 	else
 	{
 		// last->prev->type == T_WORD 인 경우
-		parse_cmd_suffix(proc_data, first, last->prev);
+		parse_cmd_suffix(data, proc_data, first, last->prev);
 		if (last->type == T_WORD)
-			parse_cmd_word(proc_data, last);
+			parse_cmd_word(data, proc_data, last);
 		else // WORD REDIR 인 경우
 			syntax_error();
 	}
 }
 
-void	parse_io_here(t_proc_data *proc_data, t_node *redir_node, t_node *limiter_node)
+void	parse_io_here(t_data *data, t_proc_data *proc_data, t_node *redir_node, t_node *limiter_node)
 {
 }
 
-void	parse_io_file(t_proc_data *proc_data, t_node *redir_node, t_node *fname_node)
+void	parse_io_file(t_data *data, t_proc_data *proc_data, t_node *redir_node, t_node *fname_node)
 {
+	t_redir	*redir;
+
+	redir = ft_calloc(1, sizeof(t_redir));
+
 	if (redir_node->type == T_LESS)
 		//...
 	else if (redir_node->type == T_GREAT)
@@ -96,38 +118,41 @@ void	parse_io_file(t_proc_data *proc_data, t_node *redir_node, t_node *fname_nod
 		//...
 }
 
-void	parse_io_redirect(t_proc_data *proc_data, t_node *redir_node, t_node *fname_node)
+void	parse_io_redirect(t_data *data, t_proc_data *proc_data,
+		t_node *redir_node, t_node *fname_node)
 {
 	if (redir_node->type == DLESS)
-		parse_io_here(proc_data, redir_node, fname_node);
+		parse_io_here(data, proc_data, redir_node, fname_node);
 	else
-		parse_io_file(proc_data, redir_node, fname_node);
+		parse_io_file(data, proc_data, redir_node, fname_node);
 }
 
-void	parse_cmd_word(t_proc_data *proc_data, t_node *node)
+void	parse_cmd_word(t_data *data, t_proc_data *proc_data, t_node *node)
 {
 	if (node->type == T_WORD)
-		lst_append(proc_data->cmd_lst, new_node((void *)node->content->value));
+		lst_append(&proc_data->cmd_lst, new_node((void *)node->content->value));
 }
 
-void	parse_simple_cmd(t_list *proc_data_lst, t_node *first, t_node *last)
+void	parse_simple_cmd(t_data *data, t_node *first, t_node *last)
 {
 	t_proc_data	*proc_data;
 	t_node		*cur_node;
 
 	if (first->prev == last) // 재귀로 들어올 때 last == cur_node 인 경우. ex) ls |
 		syntax_err();
-	init_proc_data(proc_data); // calloc 0
-	lst_append(proc_data_lst, new_node((void *)proc_data));
+	proc_data = ft_calloc(1, sizeof(t_proc_data));
+	lst_init(&proc_data->cmd_lst);
+	lst_init(&proc_data->redir_lst);
+	lst_append(&data->proc_data_lst, new_node((void *)proc_data));
 	cur_node = first;
-	while (is_redir(cur_node))
+	while (is_redir(data, cur_node))
 		cur_node = cur_node->next->next;
-	parse_cmd_prefix(proc_data, first, cur_node->prev);
-	parse_cmd_word(proc_data, cur_node);
-	parse_cmd_suffix(proc_data, cur_node->next, last);
+	parse_cmd_prefix(data, proc_data, first, cur_node->prev);
+	parse_cmd_word(data, proc_data, cur_node);
+	parse_cmd_suffix(data, proc_data, cur_node->next, last);
 }
 
-void	parse_expression(t_list *proc_data_lst, t_node *first, t_node *last)
+void	parse_expression(t_data *data, t_node *first, t_node *last)
 {
 	t_node	*cur_node;
 	t_token	*token;
@@ -140,8 +165,8 @@ void	parse_expression(t_list *proc_data_lst, t_node *first, t_node *last)
 		token = (t_token *)cur_node->content;
 		if (token->type == T_PIPE)
 		{
-			parse_expression(proc_data_lst, first, cur_node->prev);
-			parse_simple_cmd(proc_data_lst, cur_node->next, last);
+			parse_expression(data, first, cur_node->prev);
+			parse_simple_cmd(data, cur_node->next, last);
 			return ;
 		}
 		cur_node = cur_node->prev;
