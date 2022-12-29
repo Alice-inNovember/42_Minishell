c28051178c79dd75bfd8022514de73a360fb8a37
junlee2 <junlee2@student.42seoul.kr>
2022-12-29 13:45:13 +0900

    Fix : t_data *data -> t_list *envp_list - junlee2

--- Project_Dir/includes/envp.h
+++ Project_Dir/includes/envp.h
@@ -6,7 +6,7 @@
 /*   By: junlee2 <junlee2@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 13:36:42 by junlee2           #+#    #+#             */
-/*   Updated: 2022/12/28 14:09:29 by junlee2          ###   ########seoul.kr  */
+/*   Updated: 2022/12/29 13:43:50 by junlee2          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,10 @@
 
 # include "data.h"
 
-void	envp_delete(t_data *data, char *key);
-void	envp_init(t_data *data, char **envp);
-void	envp_add(t_data *data, char *key, char *value);
-char	*envp_find(t_data *data, char *key);
+void	envp_delete(t_list *envp_list, char *key);
+void	envp_init(t_list *envp_list, char **envp);
+void	envp_add(t_list *envp_list, char *key, char *value);
+char	*envp_find(t_list *envp_list, char *key);
+char	**get_envp(t_list *envp_list);
 
 #endif
