367065bf02174bef1196e7b594953b9e06bfd574
Clearsu <clearwater1219@gmail.com>
2023-01-02 22:18:57 +0900

    Fix : removed sub_token_lst() from util.h - jincpark

--- Project_Dir/includes/util.h
+++ Project_Dir/includes/util.h
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 18:44:41 by jincpark          #+#    #+#             */
-/*   Updated: 2023/01/02 21:06:59 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/01/02 22:18:33 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,5 @@ void	del_s_token(void **content);
 void	del_s_cmd(void **content);
 void	del_s_redir(void **content);
 void	del_s_proc_data(void **content);
-
-t_list	*sub_token_lst(t_data *data, t_node *first, t_node *last);
 
 #endif
