1a43e45b88b53584daab5e60ef1e4c32616e90f3
Clearsu <clearwater1219@gmail.com>
2023-01-02 21:07:56 +0900

    Fix : fixed prototype of del_s_proc_data() - jincpark

--- Project_Dir/includes/util.h
+++ Project_Dir/includes/util.h
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 18:44:41 by jincpark          #+#    #+#             */
-/*   Updated: 2023/01/02 20:50:00 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/01/02 21:06:59 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 void	del_s_token(void **content);
 void	del_s_cmd(void **content);
 void	del_s_redir(void **content);
-void	del_proc_data(void **content);
-void	del_proc_data(void **content);
-t_list	*sub_token_lst(t_node *first, t_node *last);
+void	del_s_proc_data(void **content);
+
+t_list	*sub_token_lst(t_data *data, t_node *first, t_node *last);
 
 #endif
