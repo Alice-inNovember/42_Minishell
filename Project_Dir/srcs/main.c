af14805368b18858371cb6afcab4ce433d8af330
junlee2 <junlee2@student.42seoul.kr>
2023-01-02 15:52:52 +0900

    Rename : builtin_util.c -> built_in_util.c - junlee2

--- Project_Dir/srcs/main.c
+++ Project_Dir/srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minseok2 <minseok2@student.42seoul.kr      +#+  +:+       +#+        */
+/*   By: junlee2 <junlee2@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 19:13:42 by minseok2          #+#    #+#             */
-/*   Updated: 2022/12/30 17:47:03 by minseok2         ###   ########.fr       */
+/*   Updated: 2023/01/02 13:37:31 by junlee2          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,8 @@ int	main(int argc, char **argv, char **envp)
 		print_token_lst(&data.token_lst);
 		lst_init(&data.proc_data_lst);
 		data.syntax_err_flag = 0;
-		parse_expression(&data, data.token_lst.head->next, data.token_lst.tail->prev);
+		parse_expression(&data, \
+		data.token_lst.head->next, data.token_lst.tail->prev);
 		if (data.syntax_err_flag == 1)
 			continue ;
 		lst_clear(&data.token_lst, del_token);
