579a94c94ff3f73d68cc327cdd567fb50f0ed800
Clearsu <clearwater1219@gmail.com>
2023-01-02 22:15:50 +0900

    Fix : fixed prototype of parse_expression() - jincpark

--- Project_Dir/includes/parser.h
+++ Project_Dir/includes/parser.h
@@ -6,14 +6,14 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 18:23:08 by jincpark          #+#    #+#             */
-/*   Updated: 2022/12/29 18:44:20 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/01/02 22:15:13 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
 
-void	parse_expression(t_data *data, t_node *first, t_node *last);
+void	parse_expression(t_data *data, t_list *token_lst);
 void	syntax_err(t_data *data);
 
 #endif
