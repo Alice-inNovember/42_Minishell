610c6769b31c541cb3de2a710350b2533575c901
Clearsu <clearwater1219@gmail.com>
2023-01-02 21:17:09 +0900

    Fix : fixed syntax_err() - jinpark

--- Project_Dir/srcs/util/syntax_err.c
+++ Project_Dir/srcs/util/syntax_err.c
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 14:48:29 by jincpark          #+#    #+#             */
-/*   Updated: 2023/01/02 14:51:24 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/01/02 21:16:14 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 
 void	syntax_err(t_data *data)
 {
-	data->syntax_err_flag = 1;
-	lst_clear(&data->token_lst, del_s_token);
-	lst_clear(&data->proc_data_lst, del_proc_data);
-	printf("error : syntax error\n");
+	if (data->syntax_err_flag == 0)
+	{
+		data->syntax_err_flag = 1;
+		lst_clear(&data->token_lst, del_s_token);
+		lst_clear(&data->proc_data_lst, del_proc_data);
+		printf("error : syntax error\n");
+	}
 }
