3145b803e0bfb0a7d9d4943d3a0719423559871b
tg201005 <teemo20001005@gmail.com>
2023-01-02 16:31:05 +0900

    Feat : void () -> int() for error managing

--- Project_Dir/srcs/builtin/built_in.c
+++ Project_Dir/srcs/builtin/built_in.c
@@ -6,7 +6,7 @@
 /*   By: tyi <tyi@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 19:06:49 by tyi               #+#    #+#             */
-/*   Updated: 2022/12/30 20:37:46 by tyi              ###   ########.fr       */
+/*   Updated: 2023/01/02 15:40:15 by tyi              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 # include <sys/types.h>
 # include <dirent.h>
 # include "./../../libraries/dllist/includes/dllist.h"
+
 
 
 int	check_word_cnt(char **cmd_vector)
@@ -53,7 +54,7 @@ void	bt_echo(char **cmd_vector, t_list *envp_list)
 }
 
 
-void	bt_cd(char **cmd_vector, t_list *envp_list)
+int	bt_cd(char **cmd_vector, t_list *envp_list)
 {
 	char	*path;
 	char	*new_pwd;
@@ -67,27 +68,31 @@ void	bt_cd(char **cmd_vector, t_list *envp_list)
 		error_handle();
 	old_pwd = getcwd(0, 0);
 	if (chdir(path))
+	{
 		error_handle();
+		return (1);
+	}
 	new_pwd = getcwd(0, 0);
 	envp_delete(envp_list, "PWD");
 	envp_add(envp_list, "PWD", new_pwd);
-	if (envp_find(envp_list, "OLDPWD"))
-		envp_delete(envp_list, "OLDPWD");
+	envp_delete(envp_list, "OLDPWD");
 	envp_add(envp_list, "OLDPWD", old_pwd);
+	return (0);
 }
 
 
 
 
 
-void	bt_pwd(char **cmd_vector, t_list *envp_list)
+int	bt_pwd(char **cmd_vector, t_list *envp_list)
 {
 	char	*pwd;
 	
 	if (check_word_cnt(cmd_vector) != 1);
-		error_handle();
+		return (1);
 	pwd = envp_find(envp_list, "PWD");
 	printf("%s\n", pwd);
+	return (0);
 }
 
 // handel case
@@ -96,15 +101,15 @@ void	bt_pwd(char **cmd_vector, t_list *envp_list)
 // error case
 // env word ...
 
-void	bt_env(char **cmd_vector,t_list *envp_list)
+int	bt_env(char **cmd_vector,t_list *envp_list)
 {
 	t_node	*cur_node;
 	char	*key;
 	char	*value;
 
 	
-	if (check_word_cnt(cmd_vector) != 1);
-		error_handle();
+	if (check_word_cnt(cmd_vector) != 1)
+		return (1);
 	cur_node = lst_peek_first(envp_list);
 	while (cur_node != lst_peek_last(envp_list))
 	{
@@ -112,6 +117,8 @@ void	bt_env(char **cmd_vector,t_list *envp_list)
 		value = ((t_envp *)cur_node->content)->value;
 		printf("%s=%s\n", key, value);
 	}
+	printf("_=/usr/bin/env\n");
+	return (0);
 }
 
 void	bt_exit(char **cmd_vector,t_list *envp_list)
@@ -159,47 +166,62 @@ void	print_export(char **cmd_vector, t_list *envp_list)
 	}
 }
 
-void	bt_export(char **cmd_vector,t_list *envp_list)
+int	bt_export(char **cmd_vector,t_list *envp_list)
 {
 	int		equal_i;
 	int		word_i;
+	int		error_flag;
 	char	*word;
 	char	*key;
 	char	*value;
 
+	error_flag = 0;
 	word_i = 1;
 	while (cmd_vector[word_i]) // mid error continue
 	{
 		word = cmd_vector[word_i];
 		equal_i = ft_findchr_i(word, '=');
 		if (equal_i <= 0)
-			error_handle() ; // if with pipe execution must exit(1);
+		{
+			error_flag = 1; // if with pipe execution must exit(1);
+			continue ;
+		}
 		key = ft_substr(word, 0, equal_i + 1);
 		if (!is_proper_env(key)) // fdsfad
-			continue ;
+		{
+			error_flag = 1;
+			continue ;	
+		}
 		value = ft_substr(word, 0, ft_strlen(word) - equal_i);
+		envp_delete(envp_list);
 		envp_add(envp_list, key, value);
 		word_i++;
 	}
 	if (word_i == 1)
 		print_export(cmd_vector, envp_list);
-
+	return (error_flag);
 }
 
-
-
-void	bt_unset(char **cmd_vector, t_list *envp_list)
+int bt_unset(char **cmd_vector, t_list *envp_list)
 {
+	int		error_flag;
 	int		word_i;
 	char	*key;
+
 	
-	//error key
+	error_flag = 0;
 	word_i = 1;
 	while (cmd_vector[word_i])
 	{
+		if (!is_proper_env(cmd_vector[word_i]))
+		{
+			error_flag = 1;
+			continue ;
+		}
 		key = cmd_vector[word_i];
 		envp_delete(envp_list, key);
 	}
+	return (error_flag);
 }
 
 int	main(int ac, char **av)
