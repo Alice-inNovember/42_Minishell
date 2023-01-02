e5a899493d485f551f3a0b6edd65ba38e40f23b0
Clearsu <clearwater1219@gmail.com>
2023-01-02 20:50:20 +0900

    Feat : added sub_token_lst() in header - jincpark

--- Project_Dir/includes/util.h
+++ Project_Dir/includes/util.h
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 18:44:41 by jincpark          #+#    #+#             */
-/*   Updated: 2022/12/29 18:46:52 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/01/02 20:50:00 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@ void	del_s_cmd(void **content);
 void	del_s_redir(void **content);
 void	del_proc_data(void **content);
 void	del_proc_data(void **content);
+t_list	*sub_token_lst(t_node *first, t_node *last);
 
 #endif
