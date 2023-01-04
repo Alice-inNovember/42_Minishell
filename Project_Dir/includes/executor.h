f5fa26695494f297545a5af44d8d9f4ec9e8ab58
Clearsu <clearwater1219@gmail.com>
2023-01-04 13:35:46 +0900

    Fix : added executor() in executor.h - jincpark

--- Project_Dir/includes/executor.h
+++ Project_Dir/includes/executor.h
@@ -6,7 +6,7 @@
 /*   By: junlee2 <junlee2@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 14:00:46 by junlee2           #+#    #+#             */
-/*   Updated: 2023/01/04 12:49:30 by junlee2          ###   ########seoul.kr  */
+/*   Updated: 2023/01/04 13:35:10 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include "data.h"
 #include <cstdlib>
 
+void	executor(t_data *data);
 void	execute_child(t_data *data, t_proc_data *proc, int *pip, int read_end);
 char	**cmd_list2arr(t_list *cmd_list);
 char	**get_path(t_data *data);
