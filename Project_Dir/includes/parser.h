322b09ebe635c8cb4327c1477e9fa166dbdcb096
gerry-mandering <kimmen1119@gmail.com>
2023-01-05 13:33:20 +0900

    Fix: parser header fix, lexer index modification - minseok2

--- Project_Dir/includes/parser.h
+++ Project_Dir/includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: junlee2 <junlee2@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 18:23:08 by jincpark          #+#    #+#             */
-/*   Updated: 2023/01/03 13:35:33 by junlee2          ###   ########seoul.kr  */
+/*   Updated: 2023/01/05 13:29:51 by minseok2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 
 void	parse_expression(t_data *data, t_list *token_list);
 void	syntax_err(t_data *data);
-t_list	*sub_token_list(t_data *data, t_node *first, t_node *last);
+t_list	*sub_token_list(t_node *first, t_node *last);
 
 #endif
