6ddd6eda61884c0f39a8367a9c8f120ae795b366
Clearsu <clearwater1219@gmail.com>
2023-01-05 15:28:56 +0900

    Fix : parse_expression() to parser() - jincpark

--- Project_Dir/includes/parser.h
+++ Project_Dir/includes/parser.h
@@ -6,14 +6,14 @@
 /*   By: junlee2 <junlee2@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 18:23:08 by jincpark          #+#    #+#             */
-/*   Updated: 2023/01/05 13:29:51 by minseok2         ###   ########.fr       */
+/*   Updated: 2023/01/05 15:16:49 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
 
-void	parse_expression(t_data *data, t_list *token_list);
+void	parser(t_data *data);
 void	syntax_err(t_data *data);
 t_list	*sub_token_list(t_node *first, t_node *last);
 
