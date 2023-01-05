250e91620b906e9588bac06f650b12ecfd199b31
Clearsu <clearwater1219@gmail.com>
2023-01-05 16:40:47 +0900

    Add : splitted parser.c to 5 files - jincpark

---
+++ Project_Dir/srcs/parser/parse_suffix_prefix.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_suffix_prefix.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/05 16:25:02 by jincpark          #+#    #+#             */
+/*   Updated: 2023/01/05 16:38:49 by jincpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	parse_cmd_prefix(t_data *data, t_proc_data *proc_data, t_list *token_list)
+{
+	t_node	*first;
+	t_node	*last;
+
+	if (is_return_case(data, token_list, E_NONE))
+		return ;
+	first = list_peek_first_node(token_list);
+	last = list_peek_last_node(token_list);
+	parse_cmd_prefix(data, proc_data, sub_token_list(first, last->prev->prev));
+	parse_io_redirect(data, proc_data, sub_token_list(last->prev, last));
+	list_clear_and_free(token_list, del_s_token);
+}
+
+void	parse_cmd_suffix(t_data *data, t_proc_data *proc_data, t_list *token_list)
+{
+	t_node	*first;
+	t_node	*last;
+
+	if (is_return_case(data, token_list, E_NONE))
+		return ;
+	first = list_peek_first_node(token_list);
+	last = list_peek_last_node(token_list);
+	if (is_redir_form(data, last->prev))
+	{
+		parse_cmd_suffix(data, proc_data, sub_token_list(first, last->prev->prev));
+		parse_io_redirect(data, proc_data, sub_token_list(last->prev, last));
+		list_clear_and_free(token_list, del_s_token);
+		
+	}
+	else
+	{
+		parse_cmd_suffix(data, proc_data, sub_token_list(first, last->prev));
+		if (is_redir(((t_token *)last->content)->type))
+			data->syntax_err_flag = E_NEAR_NEWLINE;
+		parse_cmd_word(data, proc_data, sub_token_list(last, last));
+		list_clear_and_free_and_free(token_list, del_s_token);
+	}
+}
--- Project_Dir/srcs/parser/parser_utils.c
+++ Project_Dir/srcs/parser/parser_utils.c
@@ -6,16 +6,11 @@
 /*   By: junlee2 <junlee2@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 19:13:38 by jincpark          #+#    #+#             */
-/*   Updated: 2023/01/05 13:07:47 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/01/05 16:22:39 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-static int	is_head_or_tail(t_node *first, t_node *last)
-{
-	return (first->next == NULL || last->prev == NULL);
-}
 
 t_list	*sub_token_list(t_node *first, t_node *last)
 {
@@ -23,7 +18,8 @@ t_list	*sub_token_list(t_node *first, t_node *last)
 	t_node	*curr;
 	t_token	*token_cp;
 
-	if (first == NULL || last == NULL || is_head_or_tail(first, last))
+	if (first == NULL || last == NULL
+		|| first->next == NULL || last->prev == NULL)
 		return (NULL);
 	new_list = ft_calloc(1, sizeof(t_list));
 	list_init(new_list);
@@ -38,4 +34,35 @@ t_list	*sub_token_list(t_node *first, t_node *last)
 		curr = curr->next;
 	}
 	return (new_list);
+}
+
+void	list_clear_and_free(t_list *list, void del(void *))
+{
+	list_clear(list, del);
+	free(list);
+}
+
+t_token	*get_token(t_node *node)
+{
+	return ((t_token *)node->content);
+}
+
+t_proc_data	*new_proc_data(void)
+{
+	t_proc_data	*new_proc_data;
+
+	new_proc_data = ft_calloc(1, sizeof(t_proc_data));
+	list_init(&new_proc_data->cmd_list);
+	list_init(&new_proc_data->redir_list);
+	return (new_proc_data);
+}
+
+t_node	*get_cmd_node(t_data *data, t_list *token_list)
+{
+	t_node	*curr;
+
+	curr = list_peek_first_node(token_list);
+	while (is_redir_form(data, curr))
+		curr = curr->next->next;
+	return (curr);
 }
