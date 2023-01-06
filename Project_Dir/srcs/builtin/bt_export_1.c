328c346fc0727a485d83ccd381e8b4655757775a
tg201005 <teemo20001005@gmail.com>
2023-01-06 13:24:20 +0900

    Remove : separte export file to two

--- Project_Dir/srcs/builtin/bt_export_1.c
+++ Project_Dir/srcs/builtin/bt_export_1.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   bt_export.c                                        :+:      :+:    :+:   */
+/*   bt_export_1.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tyi <tyi@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 17:33:49 by tyi               #+#    #+#             */
-/*   Updated: 2023/01/05 23:12:07 by tyi              ###   ########.fr       */
+/*   Updated: 2023/01/06 13:09:45 by tyi              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,70 +26,7 @@ int	ft_findchr_i(char *str, char c)
 	return (-1);
 }
 
-char	**make_key_arr(t_list *envp_list)
-{
-	int		i;
-	t_node	*cur_node;
-	char	**key_arr;
-
-	i = 0;
-	key_arr = ft_calloc(list_size(envp_list) + 1, sizeof(char *));
-	cur_node = list_peek_first_node(envp_list);
-	while (cur_node->next)
-	{
-		key_arr[i] = ((t_envp *)cur_node->content)->key;
-		cur_node = cur_node->next;
-		i++;
-	}
-	key_arr[i] = 0;
-	return (key_arr);
-}
-
-void	make_order_key_arr(char **key_arr)
-{
-	int		i;
-	int		j;
-	char	*temp;
-
-	i = 0;
-	while (key_arr[i])
-	{
-		j = i + 1;
-		while (key_arr[j])
-		{	
-			if (ft_strcmp(key_arr[i], key_arr[j]) > 0)
-			{
-				temp = key_arr[i];
-				key_arr[i] = key_arr[j];
-				key_arr[j] = temp;
-			}
-			j++;
-		}
-		i++;
-	}
-}
-
-void	print_export(t_list *envp_list)
-{
-	char	**key_arr;
-	int		i;
-	char	*key;
-	char	*value;
-
-	i = 0;
-	key_arr = make_key_arr(envp_list);
-	make_order_key_arr(key_arr);
-	while (key_arr[i])
-	{
-		key = key_arr[i];
-		value = envp_find(envp_list, key);
-		printf("declare -x %s=\"%s\"\n", key, value);
-		i++;
-	}	
-	free(key_arr);
-}
-
-// commit할 떄 out
+// // commit할 떄 out
 
 // int	get_env_length(char *line)
 // {
@@ -100,7 +37,7 @@ void	print_export(t_list *envp_list)
 // 		length++;
 // 	if (length == 0)
 // 		return (0);
-// 	while (line[length] == '_' || \
+// 	while (line[length] == '_' || 
 // 			ft_isalpha(line[length]) || ft_isdigit(line[length]))
 // 		length++;
 // 	return (length);
@@ -119,7 +56,8 @@ int	is_proper_env(char *env_name)
 		return (0);
 }
 
-int	check_word_sep_key_val(char *word, char **key, char **value, int *error_flag)
+int	check_word_sep_key_val(char *word, \
+char **key, char **value, int *error_flag)
 {
 	int		equal_i;
 
