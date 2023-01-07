00d0a4189ed761c1b70ef71d62e71f16cbc5a7ee
Clearsu <clearwater1219@gmail.com>
2023-01-07 18:45:38 +0900

    Add : added prototypes of funcs - jincpark

--- Project_Dir/includes/util.h
+++ Project_Dir/includes/util.h
@@ -6,7 +6,7 @@
 /*   By: junlee2 <junlee2@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 18:44:41 by jincpark          #+#    #+#             */
-/*   Updated: 2023/01/06 14:03:18 by junlee2          ###   ########seoul.kr  */
+/*   Updated: 2023/01/07 18:39:09 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,6 @@ void	del_s_proc_data(void *content);
 void	del_s_pid(void *content);
 
 void	print_syntax_err(t_data *data);
+int		is_line_empty(char *line);
 
 #endif
