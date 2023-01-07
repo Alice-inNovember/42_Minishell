ed4453e8c658ba25bfd851527cde8d7dfb3a7a31
Clearsu <clearwater1219@gmail.com>
2023-01-07 18:46:13 +0900

    Refactor : moved functions to etc.c - jincpark

--- Project_Dir/srcs/main.c
+++ Project_Dir/srcs/main.c
@@ -6,14 +6,13 @@
 /*   By: junlee2 <junlee2@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 19:13:42 by minseok2          #+#    #+#             */
-/*   Updated: 2023/01/07 18:32:56 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/01/07 18:40:31 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include "readline/readline.h"
 #include "readline/history.h"
-#include "../includes/data.h"
 #include "../includes/lexer.h"
 #include "../includes/parser.h"
 #include "../includes/envp.h"
@@ -22,44 +21,6 @@
 #include "../includes/util.h"
 
 int		g_last_exit_status;
-
-void	init_data(t_data *data, char **envp)
-{
-	static int	first_call_flag = ON;
-
-	if (first_call_flag == ON)
-	{
-		envp_init(&data->envp_list, envp);
-		builtin_init(&data->builtin_list);
-		first_call_flag = OFF;
-	}
-	list_init(&data->token_list);
-	list_init(&data->proc_data_list);
-	list_init(&data->pid_list);
-	data->syntax_err_flag = 0;
-}
-
-void	clear_data(t_data *data)
-{
-	list_clear(&data->token_list, del_s_token);
-	list_clear(&data->proc_data_list, del_s_proc_data);
-	list_clear(&data->pid_list, free);
-}
-
-int	is_line_empty(char *line)
-{
-	char	*line_cp;
-
-	line_cp = line;
-	while (*line && ((*line >= 9 && *line <= 13) || *line == 32))
-		line++;
-	if (*line == '\0')
-	{
-		free(line_cp);
-		return (1);
-	}
-	return (0);
-}
 
 int	main(int argc, char **argv, char **envp)
 {
