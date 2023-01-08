565e9512128389aab097072acd7b4053dd041e4a
gerry-mandering <kimmen1119@gmail.com>
2023-01-08 23:10:16 +0900

    Feat: main remove comment - minseok2

--- Project_Dir/srcs/main.c
+++ Project_Dir/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: tyi <tyi@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 19:13:42 by minseok2          #+#    #+#             */
-/*   Updated: 2023/01/08 22:45:43 by minseok2         ###   ########.fr       */
+/*   Updated: 2023/01/08 23:08:23 by minseok2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,8 +41,8 @@ int	main(int argc, char **argv, char **envp)
 		add_history(data.line);
 		tokenizer(&data);
 		print_token_list(&data);
-		//parser(&data);
-		//executor(&data);
+		parser(&data);
+		executor(&data);
 		print_syntax_err(&data);
 		clear_data(&data);
 	}
