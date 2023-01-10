51d660fd68677d6f82dee644fdfc90fd2c6bed01
Clearsu <clearwater1219@gmail.com>
2023-01-10 21:29:13 +0900

    Fix : added here_flag in function reset_signal() - jincpark

--- Project_Dir/includes/signal_handler.h
+++ Project_Dir/includes/signal_handler.h
@@ -6,7 +6,7 @@
 /*   By: tyi <tyi@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 14:17:46 by jincpark          #+#    #+#             */
-/*   Updated: 2023/01/10 14:16:51 by tyi              ###   ########.fr       */
+/*   Updated: 2023/01/10 21:07:21 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 # define SIGNAL_H
 
 void	set_signal(void);
-void	reset_signal(pid_t pid);
+void	reset_signal(pid_t pid, int here_flag);
 
 #endif
--- Project_Dir/srcs/signal/signal_handler.c
+++ Project_Dir/srcs/signal/signal_handler.c
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 13:54:31 by jincpark          #+#    #+#             */
-/*   Updated: 2023/01/09 22:06:56 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/01/10 21:27:55 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,38 +36,37 @@ void	display_new_prompt(int signo)
 void	exit_proc(void)
 {
 	g_last_exit_status = 1;
+	printf("\n");
+	rl_on_new_line();
+	rl_replace_line("", 1);
 	exit(1);
 }
 
-void	clear_buffer(int signo)
+void	print_new_line(int signo)
 {
 	if (signo == SIGINT)
-	{
-		rl_replace_line("", 1);
 		printf("\n");
-	}
 }
 
 void	set_signal(void)
 {
 	rl_catch_signals = 0;
 	signal(SIGINT, display_new_prompt);
-	signal(SIGTERM, exit_proc);
 	signal(SIGQUIT, SIG_IGN);
 }
 
-void	reset_signal(pid_t pid)
+void	reset_signal(pid_t pid, int here_flag)
 {
 	if (pid == 0)
 	{
+		rl_catch_signals = 1;
 		signal(SIGINT, exit_proc);
-		signal(SIGTERM, exit_proc);
-		signal(SIGQUIT, SIG_IGN);
 	}
 	else
 	{
-		signal(SIGINT, clear_buffer);
-		signal(SIGTERM, SIG_IGN);
-		signal(SIGQUIT, SIG_IGN);
+		if (here_flag)
+			signal(SIGINT, SIG_IGN);
+		else
+			signal(SIGINT, print_new_line);
 	}
 }
