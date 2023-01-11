39b21d533e42b3486e77fba9462a460e6aeb7b26
Clearsu <clearwater1219@gmail.com>
2023-01-11 17:29:10 +0900

    Fix : removed unlink() - jincpark

--- Project_Dir/srcs/signal/signal_handler.c
+++ Project_Dir/srcs/signal/signal_handler.c
@@ -6,7 +6,7 @@
 /*   By: junlee2 <junlee2@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 13:54:31 by jincpark          #+#    #+#             */
-/*   Updated: 2023/01/11 16:09:45 by junlee2          ###   ########seoul.kr  */
+/*   Updated: 2023/01/11 17:28:52 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@
 #include "readline/readline.h"
 #include "../../includes/data.h"
 
-extern t_exit_heredoc	g_exit_heredoc;
+extern int	g_last_exit_status;
 
 void	display_new_prompt(int signo)
 {
 	if (signo == SIGINT)
 	{
-		g_exit_heredoc.exit_status = 1;
+		g_last_exit_status = 1;
 		rl_replace_line("", 1);
 		printf("\n");
 		if (rl_on_new_line() == -1)
@@ -33,12 +33,10 @@ void	display_new_prompt(int signo)
 	}
 }
 
-void	exit_proc(int signo)
+void	sigint_handler(int signo)
 {
 	if (signo == SIGINT)
 	{
-		if (g_exit_heredoc.heredoc_filename != NULL)
-			unlink(g_exit_heredoc.heredoc_filename);
 		ft_putendl_fd("", STDERR_FILENO);
 		rl_on_new_line();
 		rl_replace_line("", 1);
@@ -59,13 +57,27 @@ void	set_signal(void)
 	signal(SIGQUIT, SIG_IGN);
 }
 
-void	reset_signal_before_fork(pid_t pid)
+void	sigquit_handler(int signo)
+{
+	if (signo == SIGQUIT)
+	{
+		ft_putendl_fd("Quit: 3", STDOUT_FILENO);
+		rl_on_new_line();
+		rl_replace_line("", 1);
+		exit(EX_BY_SIGNAL);
+	}
+}
+
+void	reset_signal(pid_t pid, int here_flag)
 {
 	rl_catch_signals = 1;
 	if (pid == 0)
 	{
-		signal(SIGINT, exit_proc);
-		signal(SIGQUIT, exit_proc);
+		signal(SIGINT, sigint_handler);
+		if (here_flag)
+			signal(SIGQUIT, SIG_IGN);
+		else
+			signal(SIGQUIT, sigquit_handler);
 	}
 	else
 		signal(SIGINT, SIG_IGN);
