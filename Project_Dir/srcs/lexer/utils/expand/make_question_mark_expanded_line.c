9612ce384d01e437b573dde83440c878d8218494
gerry-mandering <kimmen1119@gmail.com>
2023-01-11 13:58:11 +0900

    Fix: fix memory leaks in dollor question mark input - minseok2

--- Project_Dir/srcs/lexer/utils/expand/make_question_mark_expanded_line.c
+++ Project_Dir/srcs/lexer/utils/expand/make_question_mark_expanded_line.c
@@ -6,7 +6,7 @@
 /*   By: minseok2 <minseok2@student.42seoul.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 22:04:10 by minseok2          #+#    #+#             */
-/*   Updated: 2023/01/08 22:11:22 by minseok2         ###   ########.fr       */
+/*   Updated: 2023/01/11 13:56:18 by minseok2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,31 @@
 
 extern int	g_last_exit_status;
 
+static char	*make_backline(char *line, int index)
+{
+	char	*backline;
+	int		backline_start_index;
+	int		backline_size;
+
+	backline_start_index = index + 2;
+	backline_size = ft_strlen(line) - backline_start_index;
+	backline = ft_substr(line, backline_start_index, backline_size);
+	return (backline);
+}
+
 char	*make_question_mark_expanded_line(t_asset *asset)
 {
 	char	*expanded_line;
 	char	*frontline;
 	char	*backline;
-	int		backline_start_index;
-	int		backline_size;
+	char	*num_str;
 
 	frontline = ft_substr(asset->line, 0, asset->index);
-	backline_start_index = asset->index + 2;
-	backline_size = ft_strlen(asset->line) - backline_start_index;
-	backline = ft_substr(asset->line, backline_start_index, backline_size);
-	expanded_line = ft_str4join(frontline, \
-								ft_itoa(g_last_exit_status), "\\", backline);
+	backline = make_backline(asset->line, asset->index);
+	num_str = ft_itoa(g_last_exit_status);
+	expanded_line = ft_str4join(frontline, num_str, "\\", backline);
 	free(frontline);
 	free(backline);
+	free(num_str);
 	return (expanded_line);
 }
