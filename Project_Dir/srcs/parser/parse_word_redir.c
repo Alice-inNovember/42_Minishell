6157e66c381fc12756fb480f44b95ab1d98359ac
Clearsu <clearwater1219@gmail.com>
2023-01-12 20:31:18 +0900

    Refactor : refactored parse_io_file() - jincpark

--- Project_Dir/srcs/parser/parse_word_redir.c
+++ Project_Dir/srcs/parser/parse_word_redir.c
@@ -6,7 +6,7 @@
 /*   By: junlee2 <junlee2@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 16:34:51 by jincpark          #+#    #+#             */
-/*   Updated: 2023/01/12 19:22:04 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/01/12 19:35:10 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,16 +30,15 @@ void	parse_io_file(t_data *data, t_proc_data *proc_data, t_list *token_list)
 {
 	t_redir	*redir;
 	t_type	type;
-	t_token	*filename_token;
+	void	*filename;
 
 	if (is_return_case(data, token_list, E_NONE))
 		return ;
 	redir = ft_calloc(1, sizeof(t_redir));
-	list_append(&proc_data->redir_list, new_node((void *)redir));
 	type = ((t_token *)list_peek_first_content(token_list))->type;
-	set_redir_type(redir, type);
-	filename_token = (t_token *)list_peek_last_content(token_list);
-	redir->filename = ft_strdup((char *)filename_token->value);
+	filename = ((t_token *)list_peek_last_content(token_list))->value;
+	set_s_redir(redir, type, (char *)filename);
+	list_append(&proc_data->redir_list, new_node((void *)redir));
 	clear_and_free_token_list(token_list);
 }
 
