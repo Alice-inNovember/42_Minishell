a8b693255cc3bba7e71fae606d7bba23928a4ba4
junlee2 <junlee2@student.42seoul.kr>
2023-01-13 09:09:23 +0900

    fix : heredoc g_exit_status - junlee2

--- Project_Dir/srcs/parser/parse_heredoc.c
+++ Project_Dir/srcs/parser/parse_heredoc.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   parse_heredoc.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tyi <tyi@student.42seoul.kr>               +#+  +:+       +#+        */
+/*   By: junlee2 <junlee2@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 17:19:30 by jincpark          #+#    #+#             */
-/*   Updated: 2023/01/11 20:59:20 by tyi              ###   ########.fr       */
+/*   Updated: 2023/01/13 09:07:58 by junlee2          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/parser.h"
+#include <stdlib.h>
 #include <unistd.h>
 
 extern int	g_exit_status;
@@ -25,15 +26,17 @@ void	parse_io_here(t_data *data, t_proc_data *proc_data, t_list *token_list)
 		return ;
 	filename = make_temp_file(data);
 	limiter = get_limiter(token_list);
-	if (get_heredoc_input(filename, limiter) == EX_BY_SIGNAL)
+	if (get_heredoc_input(filename, limiter) == EX_BY_SIGNAL + SIGINT)
 	{
-		g_exit_status = EX_BY_SIGNAL + SIGINT;
+		g_exit_status = EX_FAILURE;
 		data->syntax_err_flag = E_SIGINT;
 		clear_and_free_token_list(token_list);
 		ft_unlink(filename);
 		free(filename);
 		return ;
 	}
+	else
+		g_exit_status = EX_SUCCESS;
 	redir = (t_redir *)ft_calloc(1, sizeof(t_redir));
 	redir->type = T_DLESS;
 	redir->filename = filename;
